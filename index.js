910210f5f84af434eb0822acaa6ce77da26ae278
jim3 <j3.casey@protonmail.com>
2023-05-04 20:10:30 -0400

    update index.js

--- index.js
+++ index.js
@@ -20,18 +20,44 @@ form.addEventListener("submit", async (e) => {
     };
     console.log(data);
 
+    // POST request options: send POST request, HTTP header & json as a string to server
     const options = {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(data),
     };
 
-    // POST request to Firestore
-    const response = await fetch(url, options);
-    const responseData = await response.json();
+    const response = await fetch(url, options); // get response from server
+    const responseData = await response.json(); // convert response to json using json() method
 
     if (!responseData.error) {
         document.getElementById("create-form").reset();
     }
 
+    const insertHTML = `
+    <li class="card">
+    <div>
+    <h3 class="name">NAME</h3>
+    <div class="calories">0 calories</div>
+    <ul class="macros">
+      <li class="carbs"><div>Carbs</div><div class="value">CARBSg</div></li>
+      <li class="protein"><div>Protein</div><div class="value">PROTEINg</div></li>
+      <li class="fat"><div>Fat</div><div class="value">FATg</div></li>
+    </ul>
+    </div>
+    </li>
+`;
+
+    const foodList = document.querySelector("#food-list");
+    foodList.insertAdjacentHTML("beforeend", insertHTML);
+
+    const nameElement = foodList.querySelector(".name");
+    const carbsElement = foodList.querySelector(".carbs .value");
+    const proteinElement = foodList.querySelector(".protein .value");
+    const fatElement = foodList.querySelector(".fat .value");
+
+    nameElement.textContent = responseData.fields.name.stringValue;
+    carbsElement.textContent = `${responseData.fields.carbs.integerValue}g`;
+    proteinElement.textContent = `${responseData.fields.protein.integerValue}g`;
+    fatElement.textContent = `${responseData.fields.fat.integerValue}g`;
 });
